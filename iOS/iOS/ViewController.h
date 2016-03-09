42f100bfa2e74c688a263c51edf54a9143a52562
江武 <manager@gmail.com>
2016-03-09 17:27:24 +0800

    初始化项目

---
+++ iOS/iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS
+//
+//  Created by 江武 on 16/3/9.
+//  Copyright © 2016年 lijiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS
+//
+//  Created by 江武 on 16/3/9.
+//  Copyright © 2016年 lijiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
