425142cc3eb651eadc5398650ef4243bee378ddb
江武 <manager@gmail.com>
2016-03-11 15:42:15 +0800

    问候你好

--- iOS/iOS/ViewController.m
+++ iOS/iOS/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"你好啊！！");
+    
+    NSLog(@"呵呵哒");
 }
 
 - (void)didReceiveMemoryWarning {
